c5e1084a2da91d9f22bbf9c3cda8e38ed6ee63c8
Axel I. Garcia <agarcia@eiva.com.ar>
2021-10-15 16:30:09 -0300

    levanta servicios con docker

--- greeting-service/src/main/java/net/eivsoftware/greetingservice/GreetingController.java
+++ greeting-service/src/main/java/net/eivsoftware/greetingservice/GreetingController.java
@@ -1,13 +1,23 @@
 package net.eivsoftware.greetingservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
+	@Value("${greeting.message}")
+	private String greeting;
+	
+	@Value("${spring.datasource.driver-class-name}")
+	private String driverClassName;
+	
+	@Value("${spring.datasource.jdbc-url}")
+	private String jdbcUrl;
+	
 	@GetMapping()
 	public String greeting() {
-		return "asd";
+		return greeting + " driverClassName:" + driverClassName + " jdbcUrl:" + jdbcUrl;
 	}
 }
